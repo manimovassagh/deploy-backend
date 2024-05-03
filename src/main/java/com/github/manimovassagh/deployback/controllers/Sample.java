695e57deaaa249d3e62c8f230edbf997ad387ac7
Mani Movassagh <mani.mgh@gmail.com>
2024-05-04 01:36:46 +0200

    backend: deploy

--- src/main/java/com/github/manimovassagh/deployback/controllers/Sample.java
+++ src/main/java/com/github/manimovassagh/deployback/controllers/Sample.java
@@ -1,2 +1,16 @@
-package com.github.manimovassagh.deployback.controllers;public class Sample {
+package com.github.manimovassagh.deployback.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Sample {
+
+
+@GetMapping("/")
+    public String sample() {
+    return "Hello World!";
+}
+
 }
